6d10be3e775530a7cd547f6e2ea31765fc22abeb
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-17 20:39:45 -0500

    feat: added a base search template with minimum functionality

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ twitter_api = generate_twitter_auth()
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", title = "Twitter API Frontend")
 
-@app.route("/search/<username>")
-def search(username):
-    return ("Hello, % s" % username)
+@app.route("/search", methods = ["POST", "GET"])
+def search():
+    if request.method == "POST":
+        handle = request.form["handle"]
+        return ("Handle searched: %s" % handle)
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
