82468c6bfbe8b3132a41c9953b41d6c6dc64d8bf
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-17 22:58:26 -0500

    feat: added search page routing functionality (WIP

--- app.py
+++ app.py
@@ -14,7 +14,21 @@ def hello_world():
 def search():
     if request.method == "POST":
         handle = request.form["handle"]
-        return ("Handle searched: %s" % handle)
+        result = twitter_api.request("search/tweets", {"q": "%s" % handle, "count": 5})
+
+        if result.status_code in [200, 304]:
+            tweet_data = []
+            for item in result.get_iterator():
+                parsed_item = {
+                    "datetime": item["created_at"],
+                    "text": item["text"],
+                    "english_word_count": 0
+                }   
+                tweet_data.append(parsed_item)
+        else:
+            return render_template("error.html", message = "Give us a moment while we determine what went wrong. Thank you for your patience!")
+
+        return render_template("search.html", handle = handle, status = result.status_code, tweets = tweet_data)
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
