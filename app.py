02eaa5b3491f9a5f33ec088b3a34bb4d1a9148ad
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-17 19:59:18 -0500

    feat: added a template for the homepage; added routing to the homepage

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+from authorization import *
 
 app = Flask(__name__)
+Bootstrap(app)
+twitter_api = generate_twitter_auth()
 
 @app.route("/")
 def hello_world():
-    return "Hello, world!"
+    return render_template("index.html")
+
+@app.route("/search/<username>")
+def search(username):
+    return ("Hello, % s" % username)
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
