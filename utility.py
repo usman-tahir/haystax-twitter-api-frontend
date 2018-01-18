810930b03365646d53a9709aec9513df8d090fde
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-18 08:18:06 -0500

    feat: refactoring for the utility class

--- utility.py
+++ utility.py
@@ -1,37 +1,24 @@
 import enchant
 
-def read_words(filename):
-    words = set([word.strip("\n").lower() for word in open(filename, "r").readlines()])
-    return words
-
-def replace_all(sentence, replacements):
-    for replacement in replacements:
-        sentence = sentence.repleace(replacement, replacements[replacement])
-    return sentence
-
 def sanitize_word_input(sentence):
     symbols = [
         "!", "@", "#", "$", "%", "^", "&", "*",
         "(", ")", "_", "+", "=", "<", ">", ",",
-        "`", "~", ":". ";", "\'", "\"", "[", "]",
-        "{", "}", "|", "\\", "\/"
+        "`", "~", ":", ";", "\"", "[", "]", "{",
+        "}", "|", "\\", "\/", "?"
     ]
     replacements = {}
     
     for s in symbols:
-        replacement[s] = ""
+        replacements[s] = ""
     
     for r in replacements:
-        replacements.replace(r, replacements[r])
+        sentence = sentence.replace(r, replacements[r])
 
     return sentence
 
 def check_words(sentence):
     dictionary = enchant.Dict("en_US")
-    return len(list(dictionary.check(word) for word in sentence))
-
-def main():
-    read_words("words.txt")
-
-if __name__ == "__main__":
-    main()
+    sentence = sanitize_word_input(sentence)
+    filtered_words = list(word for word in sentence.split(" ") if len(word) > 0 and dictionary.check(word))
+    return len(filtered_words)
